54a0f0e16cdd784409e49fb7aa8f73f0966a6cd5
Дмитрий Павлов <dmitriyeternitypavlov@outlook.com>
2021-06-09 20:38:27 +0700

    Создание модели данных.

---
+++ FromSqlToExcel.Domain/Models/Links.cs
@@ -0,0 +1,14 @@
+﻿namespace FromSqlToExcel.Domain.Models
+{
+    public class Links
+    {
+        public Izdel IzdelUp { get; set; }
+        public Izdel Izdel { get; set; }
+        public int Kol { get; set; }
+
+        public override string ToString()
+        {
+            return $"Входит в состав {IzdelUp} в количестве {Kol}";
+        }
+    }
+}
---
+++ Tests/FromSqlToExcel.TestConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FromSqlToExcel.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
